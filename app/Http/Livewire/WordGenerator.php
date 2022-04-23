6274bb6bf4906019d5b46bfd7b919e743e3b8d4a
jacques <hovine.jacques@gmail.com>
2022-04-23 21:40:55 +0200

    remove debugs

--- app/Http/Livewire/WordGenerator.php
+++ app/Http/Livewire/WordGenerator.php
@@ -15,19 +15,13 @@ class WordGenerator extends Component
 
         $words = Word::all()->random()->limit($this->count)->get();
 
-        // dd($this->count);
-
         if ( empty($this->count) || $this->count === 0)
         {
             $empty = new Word();
             $empty->setContent('Inutile capitaine. Il est trop loin maintenant.');
-
-            // dd($empty);
             
             $words = collect()->add($empty);
         }
-
-        // dd($words);
 
         return view('livewire.word-generator', [
             'words' => $words,
