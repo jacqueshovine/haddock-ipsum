5810cba55b8cc496c31f8a9a8ffe2825b3f593bc
jacques <hovine.jacques@gmail.com>
2022-04-23 19:27:01 +0200

    create basic structure with index page and layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<title>Haddock Ipsum</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
+
+<body>
+    <section class="px-6 py-8">
+        <nav class="md:flex md:justify-between md:items-center">
+            <div>
+                <a href="/">
+                    Home
+                </a>
+            </div>
+        </nav>
+    </section>
+
+    {{ $slot }}
+</body>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <main>
+        <section class="py-8 max-w-4xl mx-auto">
+            <h1 class="text-3xl font-bold">Générateur de texte</h1>
+            <p class="mt-6">
+                Home
+            </p>
+        </section>
+    </main>
+</x-layout>
