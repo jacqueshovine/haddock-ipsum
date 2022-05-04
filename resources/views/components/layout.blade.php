893edf1fba1bf1b0497f44611de0664afdccfd1f
jacques <hovine.jacques@gmail.com>
2022-05-04 21:37:04 +0200

    add various style modifications

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="utf-8" />
     <title>Haddock Ipsum</title>
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/alpine.css" rel="stylesheet">
     <script src="/js/alpine.js"></script>
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
     @livewireStyles
 </head>
 
@@ -18,12 +21,20 @@
 </style>
 
 <body>
-    <section class="px-6 py-8">
-        <nav class="md:flex md:justify-between md:items-center">
+    <section class="max-w-4xl mx-auto h-32 bg-blue-700 min-h-fit md:flex md:justify-center md:items-center">
+        <header class="mx-4 p-4 ">
+            <h1 class="text-4xl">Haddock ipsum</h1>
+        </header>
+    </section>
+    <section class="pb-4 max-w-4xl mx-auto">
+        <nav class="p-4 md:flex md:justify-between md:items-center bg-blue-200 border-t-2 border-double border-orange-500">
             <div>
-                <a href="/">
-                    Home
-                </a>
+                <x-navlink class="ml-0">Haddock ipsum</x-navlink>
+                {{-- <x-navlink>Haddock mockup</x-navlink> --}}
+                <x-navlink href="/about">A propos</x-navlink>
+            </div>
+            <div>
+                <x-navlink href="https://github.com/jacqueshovine/haddock-ipsum">Github</x-navlink>
             </div>
         </nav>
     </section>
