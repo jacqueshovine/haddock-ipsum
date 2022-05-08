705336afd79beec56ef200cce8df3d329c528c9c
jacques <hovine.jacques@gmail.com>
2022-05-08 18:12:52 +0200

    move images folder to resources
    updated webpack.mix to copy images to public when compiled
    
    The goal of this is to manage all assets in the same place

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ]);
+
+mix.copy([
+    'resources/images/'
+], 'public/images');
